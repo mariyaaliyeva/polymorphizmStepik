3da3e38f56f2811a12198e196046debc696a052d
Mariya Aliyeva <mariyaalyevaseo@gmail.com>
2023-10-26 17:57:49 +0600

    m: add solutions

--- polymorphizmStepik/polymorphizmStepik/main.swift
+++ polymorphizmStepik/polymorphizmStepik/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+class Shape {
+    
+    func calculateArea() -> Double {
+        return 0
+    }
+    func calculatePerimetr() -> Double {
+        return 0
+    }
+}
 
+class Circle: Shape {
+    var radius: Double
+    
+    init(_ radius: Double) {
+        self.radius = radius
+    }
+    
+    override func calculatePerimetr() -> Double {
+        2 * .pi * radius
+    }
+    override func calculateArea() -> Double {
+        .pi * radius * radius
+    }
+    
+}
+
+class Rectangle: Shape {
+    var width: Double
+    var height: Double
+    
+    init(_ width: Double, _ height: Double) {
+        self.width = width
+        self.height = height
+    }
+    
+    override func calculatePerimetr() -> Double {
+        return width + width + height + height
+    }
+    override func calculateArea() -> Double {
+        return width * height
+    }
+}
